606f0b8baeffe8ab03af9445e11410e256b457c2
FacuPalacios <facupalacios21@gmail.com>
2026-05-04 22:15:12 -0300

    Ejercicio 3 terminado

---
+++ Dsw2026Ej8/Ejercicio3/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Ejercicio3
+{
+    public class Product
+    {
+        public string Descripcion { get; private set; }
+
+        public void SetDescripcion(string nuevaDescripcion)
+        {
+            Descripcion = nuevaDescripcion;
+        }
+    }
+}
