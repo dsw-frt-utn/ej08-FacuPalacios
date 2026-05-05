73c5d403a7ee00dd9ea76cc8d15db0c3e27d1210
FacuPalacios <facupalacios21@gmail.com>
2026-05-04 22:26:28 -0300

    Ejercicio 6 terminado

---
+++ Dsw2026Ej8/Ejercicio6/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Ejercicio6
+{
+    public static class Extensions
+    {
+        public static string ToProductCode(this string code)
+        {
+            if (code == null)
+                return "SIN-CODIGO";
+
+            string trimmed = code.Trim();
+            string upper = trimmed.ToUpper();
+            string result = upper.Replace(' ', '-');
+
+            return result;
+        }
+    }
+}
