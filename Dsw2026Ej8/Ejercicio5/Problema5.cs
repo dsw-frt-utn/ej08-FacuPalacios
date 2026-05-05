561390513b1270aa7a595e6377f423e9ae07ac6e
FacuPalacios <facupalacios21@gmail.com>
2026-05-04 22:22:20 -0300

    Ejercicio 5 terminado

---
+++ Dsw2026Ej8/Ejercicio5/Problema5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Ejercicio5
+{
+    public abstract class Sale
+    {
+        public decimal BaseAmount { get; set; }
+
+        public virtual decimal CalculateTotal()
+        {
+            return BaseAmount;
+        }
+    }
+
+    public class RetailSale : Sale
+    {
+        public override decimal CalculateTotal()
+        {
+            return BaseAmount;
+        }
+    }
+
+    public class WholesaleSale : Sale
+    {
+        public override decimal CalculateTotal()
+        {
+            return BaseAmount * 0.9m;
+        }
+    }
+
+    public class Problema5
+    {
+        public decimal ObtenerImporteFinal(Sale sale)
+        {
+            return sale.CalculateTotal();
+        }
+    }
+}
