05401aced2467d514ad9a1bb94d02a21b778df64
FacuPalacios <facupalacios21@gmail.com>
2026-05-04 22:03:58 -0300

    Ejercicio 1 terminado

---
+++ Dsw2026Ej8/Ejercicio1/ProductHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Ejercicio1
+{
+    public partial class ProductHelper
+    {
+        public string ObtenerEtiquetaProducto(long code, string description, decimal price)
+        {
+            string precioConFormato = price.ToString("C");
+            string etiqueta = $"[{code}] {description} - {precioConFormato}";
+            return etiqueta;
+        }
+    }
+}
---
+++ Dsw2026Ej8/Ejercicio1/ProductHelperAuxiliar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Ejercicio1
+{
+    public partial class ProductHelperAuxiliar
+    {
+        private const string SimboloMoneda = "$";
+        private string PrecioMoneda(decimal price)
+        {
+            return $"{SimboloMoneda}{price:N2}";
+        }
+    }
+}
